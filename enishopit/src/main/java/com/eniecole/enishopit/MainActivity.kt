ed1bf45a1462d66e001f0cd3a520dd595a6c2b87
riderofzion <quentinrouet@hotmail.fr>
2024-05-29 16:05:36 +0200

    Debut Tp ENISHOPIT

---
+++ enishopit/src/main/java/com/eniecole/enishopit/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.eniecole.enishopit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eniecole.enishopit.ui.screen.ListProductsScreen
+import com.eniecole.enishopit.ui.theme.TPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TPTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ListProductsScreen(
+                        Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
